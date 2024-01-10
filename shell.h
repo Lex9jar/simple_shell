7f79b6e97db8a140ce6c08a94993021957be8702
Lex9jar <alexanderedim89@gmail.com>
2024-01-11 00:21:40 +0100

    Header file

--- shell.h
+++ shell.h
@@ -2,6 +2,7 @@
 #define _SHELL_H_
 
 #include <stdio.h>
+#include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -25,6 +26,7 @@ size_t _strspn(char *s, const char *accept);
 size_t _strcspn(char *s, const char *reject);
 char *_strchr(const char *s, char c);
 char *trim(char *str);
+size_t word_count(char *str, const char *delim);
 
 /* 1-string_utils */
 size_t _strlen(char *str);
@@ -34,6 +36,7 @@ int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, size_t n);
 
 /* execute_commands */
-int execute_commands(char **argv, char *f_name);
+int execute_commands(char *lineptr, char *f_name);
+int _execve(char **argv, char *f_name);
 
 #endif /* _SHELL_H_ */
