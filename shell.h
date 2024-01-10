2baa9e06ed0546b062c4263af00e4d87685d4784
Lex9jar <alexanderedim89@gmail.com>
2024-01-10 11:13:15 +0100

    Header File

---
+++ shell.h
@@ -0,0 +1,28 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+#define INFINITE 1
+#define _WIFEXITED(status) (((status) & 0xFF) == 0)
+#define _WEXITSTATUS(status) (((status) >> 8) & 0xFF)
+
+/* general_tools */
+void shell_printf(char *str);
+void prompt(void);
+
+/* string_utils */
+size_t _strlen(char *str);
+char *_strcpy(char *dest, char *src);
+char *_strdup(char *str);
+int _strcmp(char *s1, char *s2);
+
+/* execute_commands */
+int execute_commands(char **argv, char *f_name);
+
+#endif /* _SHELL_H_ */
