9e0e23db7f80ea58974c450227610fa83084c47e
Lex9jar <alexanderedim89@gmail.com>
2024-01-10 22:51:03 +0100

    More string functions

--- 1-string_utils.c
+++ 1-string_utils.c
@@ -88,3 +88,25 @@ int _strcmp(char *s1, char *s2)
 	}
 	return (*s1 - *s2);
 }
+
+/**
+ * _strncpy - copies a string
+ * @dest: string to overwrite
+ * @src: string to copy
+ * @n: number of characters to copy
+ *
+ * Return: pointer to the destination string dest
+ */
+char *_strncpy(char *dest, char *src, size_t n)
+{
+	size_t i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
