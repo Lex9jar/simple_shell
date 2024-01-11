6c95bc34574eb457f98edcb6067159882814efd6
Lex9jar <alexanderedim89@gmail.com>
2024-01-11 20:17:48 +0100

    Memory Utils

--- memory_utils.c
+++ memory_utils.c
@@ -1,17 +1,47 @@
 #include "shell.h"
 
 /**
- * free_array - Frees a 2 dimensional array
- * @array: An array to free
- * @size: Size of array
+ * _realloc - reallocates memory area for a string
+ * @str: String to reallocate memory for.
+ * @size: size to allocate
+ *
+ * Return: Pointer to allocated memory, otherwise NULL
+ */
+char *_realloc(char *str, size_t size)
+{
+	size_t len = _strlen(str);
+	char *new_str;
+
+	if (len == size)
+		return (str);
+
+	new_str = malloc(len + 1);
+	if (new_str == NULL)
+		return (NULL);
+
+	new_str = _strcpy(new_str, str);
+	free(str);
+
+	return (new_str);
+}
+
+/**
+ * free_array - Frees an array of strings
+ * @array: A 2 dimensional array
+ * @size: size of the array
  *
  * Return: void.
  */
 void free_array(char **array, size_t size)
 {
-	ssize_t i = size;
+	ssize_t i;
 
-	for (; i >= 0; i--)
+	if (array[size] == NULL)
+		i = --size;
+	else
+		i = size;
+
+	for ( ; i >= 0; i--)
 		free(array[i]);
 	free(array);
 }
