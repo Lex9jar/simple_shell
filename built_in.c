2f71cb7bdc23617dc66692a13fc58869677b6ff6
Lex9jar <alexanderedim89@gmail.com>
2024-01-12 13:56:14 +0100

    built_in functions

---
+++ built_in.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+/**
+ * _printenv - Prints the environment variables
+ *
+ * Return: void.
+ */
+void _printenv(void)
+{
+	int i;
+
+	while (environ[i] != NULL)
+	{
+		s_printf(environ[i]);
+		s_printf("\n");
+	}
+}
